6bb21d10af25437872a35ee351fcc72639f383ab
mihneavlad <mihnel@yahoo.com>
2018-08-14 12:05:54 +0200

    FIRST REAL COMMIT

--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
-// External deps
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 
-// Production optimization
 import registerServiceWorker from './registerServiceWorker';
 
+import './styles/index.css';
+
+// ==========================
 // Components
-import App from './components/App';
+// ==========================
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
 
-// Styles
-import './styles/main.css';
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
 
+  render() {
+    const status = 'Next player: X';
 
-render(<App />, document.getElementById('root'));
+    return (
+      <React.Fragment>
+        <div className="status h2 text-center">{status}</div>
+        <div className="board">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 
+class Game extends React.Component {
+  render() {
+    return (
+      <article className="game container mt-5">
+        <section className="row">
+          <div className="col-sm-8 game-board">
+            <Board />
+          </div>
+          <div className="col-sm-4 game-info">
+            <p className="h2">{/* status */}</p>
+            <ul className="nav nav-pills flex-column">
+              {/* TODO */}
+            </ul>
+          </div>
+        </section>
+      </article>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById('root'));
 registerServiceWorker();
