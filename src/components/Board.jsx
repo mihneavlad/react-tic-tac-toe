6a4f6e814bd9bf99667203f4bf0537552ac16b8c
mihneavlad <mihnel@yahoo.com>
2018-08-20 14:25:42 +0200

    Updated board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -14,6 +14,11 @@ class Board extends Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+
+    if (calculateWinner(squares) || squares[i]) {
+    return;
+  }
+
     squares[i] = this.state.xIsNext ? 'X' : '0';
     this.setState({
       squares: squares,
